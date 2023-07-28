3a8e9b2d81a74fffe6912901e633e84db30e88ff
Daniel Chaves <115505626+danielmschaves@users.noreply.github.com>
2023-07-28 10:41:54 -0300

    Create setupMongo.sh

---
+++ MongoDB/setupMongo.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Install MongoDB tools if they aren't available
+# You might need to adapt these commands according to the specific environment
+# sudo apt-get install mongo-tools
+
+# Download the catalog.json file
+curl -O https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/IBM-DB0321EN-SkillsNetwork/nosql/catalog.json
